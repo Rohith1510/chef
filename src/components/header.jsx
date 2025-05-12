0a8886bf03fd03caba8946e89b735f965b7b321a
Rohith1510 <rohithpranav567@gmail.com>
2025-05-12 23:01:33 +0530

    Minor: added the header component

---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+import "../App.css";
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="src/assets/react.svg" width={60} height={60} />
+      <h1>Chef Claude</h1>
+      
+    </header>
+  );
+};
+
+export default Header;
